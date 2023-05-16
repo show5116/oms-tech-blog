a049305eec37a6bf483522f1e61d19840cc535be
show5 <show5116@naver.com>
2023-05-16 23:27:49 +0900

    modify : Seo

--- components/layout/Seo.tsx
+++ components/layout/Seo.tsx
@@ -11,6 +11,7 @@ interface IProps {
 }
 
 const Seo = React.memo(({ title, description, keywords, thumbnail = '/images/default.png' }: IProps) => {
+    const site = 'https://oms-tech-blog.netlify.app';
     const router = useRouter();
     return (
         <Head>
@@ -19,20 +20,28 @@ const Seo = React.memo(({ title, description, keywords, thumbnail = '/images/def
             {keywords && <meta name="keywords" content={keywords.join(', ')} />}
 
             <meta property="og:type" content={router.asPath === '/' ? 'blog' : 'article'} />
-            <meta property="og:url" content={process.env.NEXT_PUBLIC_SITE_URL + router.asPath} />
-            <meta property="og:title" content="{`${title} | OMS 개발블로그`}" />
-            <meta property="og:description" content="{description}" />
+            <meta property="og:url" content={site + router.asPath} />
+            {title === undefined ? (
+                <meta property="og:title" content="OMS 개발 블로그" />
+            ) : (
+                <meta property="og:title" content={`${title} | OMS 개발블로그`} />
+            )}
+            <meta property="og:description" content={description} />
             <meta property="og:site_name" content="OMS 개발블로그" />
             <meta property="og:locale" content="ko_KR" />
-            <meta property="og:image" content="{thumbnail}" />
+            <meta property="og:image" content={thumbnail} />
             <meta property="og:image:width" content="1200" />
             <meta property="og:image:height" content="630" />
 
             <meta name="twitter:card" content="summary_large_image" />
-            <meta name="twitter:title" content="{`${title} | OMS 개발블로그`}" />
-            <meta name="twitter:description" content="{description}" />
+            {title === undefined ? (
+                <meta property="twitter:title" content="OMS 개발 블로그" />
+            ) : (
+                <meta property="twitter:title" content={`${title} | OMS 개발블로그`} />
+            )}
+            <meta name="twitter:description" content={description} />
             <meta name="twitter:site" content="OMS 개발블로그" />
-            <meta name="twitter:url" content={process.env.NEXT_PUBLIC_SITE_URL + router.asPath} />
+            <meta name="twitter:url" content={site + router.asPath} />
             <meta name="twitter:image" content={thumbnail} />
         </Head>
     );
